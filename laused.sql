45ae22e80c8980406dfef98f6d169040f6ad5dff
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-03-28 11:21:46 +0200

    Adds Age as numbers

--- laused.sql
+++ laused.sql
@@ -148,7 +148,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--Adds Age as numbers
 select sum(cast(Age as int)) from Person
 
 --?
