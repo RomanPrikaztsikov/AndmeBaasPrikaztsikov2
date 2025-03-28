01695ac04b393c0f60afd7f1763f6994da78a743
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-03-28 11:12:00 +0200

    select that shows names that dont start with w,a,s

--- laused.sql
+++ laused.sql
@@ -129,7 +129,7 @@ select * from Person where Email not like '%@%'
 -- ainult üks täht
 select * from Person where Email like '_@_.com'
 
---?
+--shows names that dont start with w,a,s
 select * from Person where Name like '[^WAS]%'
 --- ?
 select * from Person where (City = 'Gotham' or City = 'New York')
