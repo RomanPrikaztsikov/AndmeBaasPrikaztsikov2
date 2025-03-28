73e6005fcb05c01fcde3a0a4b66f7c17bc99424d
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-03-28 10:54:29 +0200

    --adding constraint to the Person table Age > 0 & Age < 150

--- laused.sql
+++ laused.sql
@@ -86,7 +86,7 @@ update Person
 set Age = 149
 where Id = 8
 
---?
+--adding constraint to the Person table Age > 0 & Age < 150
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 150)
 
